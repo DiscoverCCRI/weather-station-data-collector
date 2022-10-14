1112ff76fac78ba07f209ec90f830a89a231fcfa
akielaries <akiel@akiel.org>
2022-10-13 21:53:30 -0700

    beginning data visualizing tests for the Seeed SenseCAP ONE S900 Compact Weather Station

---
+++ weather-station-viz.py
@@ -0,0 +1,10 @@
+"""
+This file will test visualizing the data collected from the 
+Seeed SenseCAP ONE S900 Compact Weather Station
+"""
+
+def main():
+    print("data visualization")
+
+if __name__ == '__main__':
+    main()
